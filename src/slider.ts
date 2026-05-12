9d479222c9cbd5c7460880e7dc45ea35005aeaf2
Dirga Yuza <dirgayuza123@gmail.com>
2026-05-13 00:06:07 +0700

    index: Remove slider element

--- src/slider.ts
+++
@@ -1,8 +0,0 @@
-let slider = document.getElementById("length") as HTMLInputElement
-let output = document.getElementById("lengthOutput") as HTMLSpanElement
-
-output.innerHTML = slider.value
-
-slider.oninput = () => {
-    output.innerHTML = slider.value
-}
